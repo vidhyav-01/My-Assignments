138684d9e24839ac8a6fccbf3bfd5c28094ef6b0
vidhyav-01 <vidhupn2829@gmail.com>
2026-02-09 09:34:00 +0530

    Add files via upload
    Week2 Day1 Home assignements-Pls review

---
+++ CreateAccount.java
@@ -0,0 +1,29 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateAccount {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver(); 
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElement(By.id("username")).sendKeys("democsr2");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM")).click();
+		driver.findElement(By.linkText("Accounts")).click();
+		driver.findElement(By.linkText("Create Account")).click();
+		driver.findElement(By.id("accountName")).sendKeys("Test Automation_vv");
+		driver.findElement(By.name("description")).sendKeys("Selenium Automation Tester");
+		driver.findElement(By.id("numberEmployees")).sendKeys("2");
+		driver.findElement(By.id("officeSiteName")).sendKeys("LeafTaps");
+		driver.findElement(By.className("smallSubmit")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.close();
+
+	}
+
+}
